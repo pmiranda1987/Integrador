37dfa22aac4a86bc74eeafa044c04540b3b880c4
Sergio Roman <sergiosk357@egmail.com>
2025-06-04 14:55:57 -0500

    hola

--- src/main/java/com/mycompany/integrador/Integrador.java
+++ src/main/java/com/mycompany/integrador/Integrador.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.integrador;
 
 import Formulario.Formulario;
 
-/**
- *
- * @author LENOVO
- */
 public class Integrador {
 
     public static void main(String[] args) {
-            Formulario f = new Formulario ();
-            f.setVisible(true);
-      
+        Formulario f = new Formulario();
+        f.setVisible(true);
+
     }
 }
- 
---
+++ src/main/java/com/mycompany/integrador/SergioRoman.java
@@ -0,0 +1,19 @@
+package com.mycompany.integrador;
+
+public class SergioRoman {
+    String nombre;
+    int edad;
+
+    public SergioRoman(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
